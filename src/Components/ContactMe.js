69e201c60464611011ab3d26f6fba261dcc87cb5
Cory Anderson <cory.anderson6@yahoo.com>
2023-05-12 13:31:35 -0500

    updated .env and API request

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -37,7 +37,7 @@ const Contact = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3001/send-message', {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/send-message`, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
