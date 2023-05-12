18127fba509f8181d261c85828e891a1ac5db57c
Cory Anderson <cory.anderson6@yahoo.com>
2023-05-12 10:48:14 -0500

    Completed back end/Designed Contact Form

---
+++ server.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    
+    pass: process.env.EMAIL_PASS,
+    
+  },
+});
+
+app.get('/api/projects', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM projects');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.post('/send-message', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Message from ${name} - ${email}`,
+      text: message,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // Insert the message into the messages table
+    const insertMessageQuery = `
+      INSERT INTO messages (name, email, message) VALUES ($1, $2, $3)
+    `;
+    const insertResult = await pool.query(insertMessageQuery, [name, email, message]);
+    console.log('Insert result:', insertResult);
+
+    res.status(200).json({ message: 'Email sent successfully.' });
+  } catch (error) {
+    console.error('Error in /send-message:', error);
+    res.status(500).json({ message: 'Error sending email.', error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,10 +1,115 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Button, TextField, Typography, Container, Snackbar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MuiAlert from '@material-ui/lab/Alert';
 
-const ContactMe = () => (
-  <div>
-    <h2>Contact Me</h2>
-    <p>Your contact information goes here...</p>
-  </div>
-);
 
-export default ContactMe;
+const useStyles = makeStyles((theme) => ({
+  contactContainer: {
+    paddingTop: '32px',
+    marginTop: '64px',
+  },
+  contactForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+  },
+  contactTextField: {
+    width: '100%',
+  },
+  contactButton: {
+    marginTop: '16px',
+  },
+}));
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const Contact = () => {
+  const classes = useStyles();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/send-message', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ name, email, message }),
+});
+
+
+      if (response.ok) {
+        setName('');
+        setEmail('');
+        setMessage('');
+        setOpen(true);
+      } else {
+        throw new Error('Failed to send message');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Container maxWidth="sm" className={classes.contactContainer}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Contact
+      </Typography>
+      <form onSubmit={handleSubmit} className={classes.contactForm}>
+        <TextField
+          label="Name"
+          variant="outlined"
+          required
+          className={classes.contactTextField}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          required
+          className={classes.contactTextField}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Message"
+          variant="outlined"
+          multiline
+          rows={4}
+          required
+          className={classes.contactTextField}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button type="submit" variant="contained" color="primary" className={classes.contactButton}>
+          Submit
+        </Button>
+      </form>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Message sent successfully!
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
+};
+
+export default Contact;
